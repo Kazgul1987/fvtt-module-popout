649bb93294b4ed8a526d0e54052553a0b0b1ec0e
Kazgul1987 <71765137+Kazgul1987@users.noreply.github.com>
2025-08-23 17:39:49 +0200

    Remove PF2e inline-roll dynamic import

--- popout.js
+++ popout.js
@@ -1553,14 +1553,8 @@ class PopoutModule {
   }
 }
 
-Hooks.once("setup", async () => {
-  if (game.system.id === "pf2e") {
-    const path = foundry.utils.getRoute(
-      "systems/pf2e/scripts/ui/inline-roll-links.mjs",
-    );
-    const { InlineRollLinks } = await import(path);
-    InlineRollLinks.activatePF2eListeners();
-  }
+Hooks.once("setup", () => {
+  if (game.system.id === "pf2e") InlineRollLinks.activatePF2eListeners();
 });
 
 Hooks.on("ready", () => {
